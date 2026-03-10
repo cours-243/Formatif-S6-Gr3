378281847d36c1d6b09ddd717eebe410b801dbc8
Carlier Benjamin <benjamincarlier.svt@gmail.com>
2026-03-09 20:32:27 -0400

    creation package Algue et sa classe
    ajout de la classe algue et sa methode ToString()

---
+++ src/CarlierBenajmin_E04/algues/Algue.java
@@ -0,0 +1,27 @@
+package CarlierBenajmin_E04.algues;
+
+/**
+ * 420-266-H26 – POO II
+ * TP1 - Heritage
+ * Nom : Carlier
+ * Prénom: Benjamin
+ */
+public class Algue {
+    //attributs
+
+    //constructeur
+    
+    //getter
+
+    //setter
+
+    //methodes utiles
+
+    //affichage
+
+
+    @Override
+    public String toString() {
+        return "Algue{}";
+    }
+}
